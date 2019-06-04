1c911bcce20f2e79f5cd250847f13c26cd32e6ab
dhiyabak <dhiyabak@gmail.com>
2019-06-04 16:21:10 +0200

    shopping cart done and root problems solved vews done without bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-
+  root to: 'products#index'
   resources :products
-  resources :order_items,
-  resource :cart, only: [:show]
-
+  resources :order_items
+  resources :cart, only: [:show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
