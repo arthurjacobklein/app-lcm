99205fad260ece215b3fbd6717d9462bb592b6d6
dhiyabak <dhiyabak@gmail.com>
2019-06-04 13:11:42 +0200

    shopping cart done

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,24 @@ class Order < ApplicationRecord
   belongs_to :account
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_item
+  before_save :update_total
+  before_create :update_status
+
+  def calculate_total
+    self.order_items.collect { |item| item.product.price * item.quantity }.sum
+  end
+
+  private
+
+  def update_status
+    if self.status == nil?
+      self.status = "In progress"
+    end
+  end
+
+  def update_total
+    self.total_price = calculate_total
+  end
+
+
 end
