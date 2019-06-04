2a5d53a612ee924ede449217785ca8bde6ff39df
arthurjacobklein <48925072+arthurjacobklein@users.noreply.github.com>
2019-06-04 16:29:38 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   resources :order_items
   resources :cart, only: [:show]
 
-  get 'home/index'
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
