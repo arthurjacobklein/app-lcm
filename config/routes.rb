a81a03f979f5d7dae5086c1e05a4cfa5f1fc4a1c
forkhembo <forkhembo@riseup.net>
2019-06-05 13:25:04 +0200

    change routes for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
 
-  root to: 'products#index'
+  get 'home/index'
+  root to: 'home#index'
+  resources :home, only: [:index]
   resources :products
   resources :order_items
   resources :cart, only: [:show]
