73acf1197b027e84d89619680051a0c91a5ff80a
dhiyabak <dhiyabak@gmail.com>
2019-06-06 13:04:42 +0200

    strip done and tested with test keys

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 
 
-
-  get 'admins/new'
-  get 'admins/create'
-  get 'admins/show'
+  resources :admins, only: [:new, :create, :show]
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   namespace :admin do
@@ -14,21 +11,17 @@ Rails.application.routes.draw do
       resources :orders
       resources :order_items
       resources :products
-
       root to: "brands#index"
     end
   devise_for :brands
 
+  resources :charges
   resources :home, only: [:index]
   resources :users, only: [:create]
-
   root to: 'products#index'
   resources :products
   resources :order_items
   resources :cart, only: [:show]
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
-
