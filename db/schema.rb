70811397d10b671951783db0c24ec6ab987194a2
ajklein <arthurjacobklein@gmail.com>
2019-06-07 11:55:37 +0200

    pb de migration activestorage réglé

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_06_05_173304) do
 
   # These are extensions that must be enabled in order to support this database
@@ -20,21 +19,6 @@ ActiveRecord::Schema.define(version: 2019_06_05_173304) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +41,18 @@ ActiveRecord::Schema.define(version: 2019_06_05_173304) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
   create_table "brands", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -65,13 +61,11 @@ ActiveRecord::Schema.define(version: 2019_06_05_173304) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.string "first_name"
     t.string "last_name"
     t.string "company_name"
     t.integer "phone_number"
     t.string "website"
-
     t.index ["email"], name: "index_brands_on_email", unique: true
     t.index ["reset_password_token"], name: "index_brands_on_reset_password_token", unique: true
   end
