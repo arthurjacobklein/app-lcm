2a479042003b2f4b4a5f5ef1dd1f42a9618a5407
forkhembo <forkhembo@riseup.net>
2019-06-14 12:55:40 +0200

    remove phone number

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :company_name, :phone_number, :website)
+      params.require(:user).permit(:first_name, :last_name, :company_name, :website)
     end
 
 
