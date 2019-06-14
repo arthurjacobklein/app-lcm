7eaf2e58a2ed13a9f02b02b9a1d593040befab31
forkhembo <forkhembo@riseup.net>
2019-06-14 12:41:39 +0200

    migration and seed on

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_102213) do
+ActiveRecord::Schema.define(version: 2019_06_13_143318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,14 @@ ActiveRecord::Schema.define(version: 2019_06_13_102213) do
     t.string "website"
     t.index ["email"], name: "index_brands_on_email", unique: true
     t.index ["reset_password_token"], name: "index_brands_on_reset_password_token", unique: true
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "order_items", force: :cascade do |t|
